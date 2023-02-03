e454253ee85658a69e7dbfd932d300da14a03b67
briri <brian.riley@ucop.edu>
2023-02-03 11:29:57 -0800

    Added shell script to upload cloudfront index.html and added hook to do so. started new script for sam build/deploy

--- src/cloudfront/upload.sh
+++ src/cloudfront/upload.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+FILE=s3_bucket_query.json
+HTML=index.html
+
 ARN_PREFIX=arn:aws:s3:::
 
-if [ $# -ne 2 ]; then
-  echo 'Wrong number of arguments. Expecting 1: The `env` for your samconfig.toml'
+if [ $# -ne 1 ]; then
+  echo 'Wrong number of arguments. Expecting 1: S3 Bucket name. Note the bucket name can be a partial name.'
   exit 2
 fi
-
-echo "Fetching resource ARNs needed for SAM template.yaml"
-
-# aws sam --parameter-overrides ParameterKey=Key1,ParameterValue=value1 ParameterKey=Key2,ParameterValue=value2
 
 if [ -f "$FILE" ]; then
   if [ -f "$HTML" ]; then
@@ -21,10 +20,10 @@ if [ -f "$FILE" ]; then
         echo "Detected S3 Bucket: $name"
 
         aws s3 cp $HTML $name
-        exit 1
+        exit 1  
       fi
     done
-
+    
     echo "No S3 buckets matched the name you provided: $1"
   else
     echo "Expecting to find an index.html in this directory!"
@@ -32,3 +31,4 @@ if [ -f "$FILE" ]; then
 else
   echo "Expecting to find a JSON query file, $FILE, for AWS CLI command `resource-groups` query~"
 fi
+
