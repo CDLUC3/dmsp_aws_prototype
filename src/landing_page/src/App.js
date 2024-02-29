cf463fe895a2c46fc6d4804f596e3d93139aac87
Juliet Shin <juliet.shin@ucop.edu>
2024-02-29 15:19:48 -0800

    added <main/> back to App.js since it broke css. Also, remove space in landing.js

--- src/landing_page/src/App.js
+++ src/landing_page/src/App.js
@@ -7,12 +7,14 @@ import PageNotFound from "./pages/not_found/not_found";
 function App() {
   return (
     <div id="App">
+      <main>
       <BrowserRouter>
         <Routes>
           <Route path="/dmps/*" element={<Landing />}/>
           <Route path="*" element={<PageNotFound/>} />
         </Routes>
       </BrowserRouter>
+      </main>
     </div>
   );
 }
