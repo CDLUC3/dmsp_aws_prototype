3fe883defe28c65585aeabea4308d48a7375dfea
Juliet Shin <juliet.shin@ucop.edu>
2024-02-29 14:41:33 -0800

    simplified property assignments in landing.js to avoid the recursive function and use optional chaining instead. Added an abort controller in fetching the data and a try-catch.

--- src/landing_page/src/App.js
+++ src/landing_page/src/App.js
@@ -1,50 +1,18 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home/home";
 import Landing from "./pages/landing/landing";
-import Dashboard from "./pages/dashboard/dashboard";
-import { Authenticate, Logout, Unauthorized } from "./Auth";
-
 import PageNotFound from "./pages/not_found/not_found";
-
-const router = createBrowserRouter([
-  {
-    path: "/dmps/*",
-    element: <Landing />,
-  },
-  {
-    path: "/unauthorized",
-    element: <Unauthorized />,
-  },
-  {
-    path: "/authenticate",
-    element: <Authenticate />,
-  },
-  {
-    path: "/logout",
-    element: <Logout />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "*",
-    element: <PageNotFound />,
-  }
-]);
 
 function App() {
   return (
     <div id="App">
-      <main>
-        <RouterProvider router={router} />
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dmps/*" element={<Landing />}/>
+          <Route path="*" element={<PageNotFound/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/landing_page/src/utils.js
+++ src/landing_page/src/utils.js
@@ -18,17 +18,6 @@ export const SanitizeHTML = ({ html, options }) => (
   <div dangerouslySetInnerHTML={sanitize(html, options)} />
 );
 
-export function getValue(obj, path, defaultNone) {
-  if (typeof defaultNone === 'undefined') defaultNone = "";
-  if (typeof path === 'string') path = path.split(".");
-
-  if (path.length === 0) throw "Path Length is Zero";
-  if (path.length === 1) return obj[path[0]];
-
-  if (!obj[path[0]]) return defaultNone;
-  return getValue(obj[path[0]], path.slice(1));
-};
-
 export function inDevMode() {
   return window.location.hostname === 'localhost';
 }
